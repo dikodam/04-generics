366a5e5c3ce5a0ef59a3b68aa1f06bd59562908c
dikodam <dikodam@googlemail.com>
2019-04-11 18:07:20 +0200

    generics implemented; SimpleList::filter, SimpleList::map as default methods in SimpleList

--- src/main/java/ohm/softa/a04/SimpleList.java
+++ src/main/java/ohm/softa/a04/SimpleList.java
@@ -1,19 +1,41 @@
 package ohm.softa.a04;
 
-public interface SimpleList extends Iterable {
-	/**
-	 * Add a given object to the back of the list.
-	 */
-	void add(Object o);
+import java.util.function.Function;
 
-	/**
-	 * @return current size of the list
-	 */
-	int size();
-
-	/**
-	 * Generate a new list using the given filter instance.
-	 * @return a new, filtered list
-	 */
-	SimpleList filter(SimpleFilter filter);
+public interface SimpleList<T> extends Iterable<T> {
+    /**
+     * Add a given object to the back of the list.
+     */
+    void add(T o);
+    
+    /**
+     * @return current size of the list
+     */
+    int size();
+    
+    T get(int index);
+    
+    /**
+     * Get a new SimpleList instance with all items of this list which match the given filter
+     *
+     * @param filter SimpleFilter instance
+     * @return new SimpleList instance
+     */
+    default SimpleList<T> filter(SimpleFilter<T> filter) {
+        SimpleList<T> result = new SimpleListImpl<>();
+        for (T o : this) {
+            if (filter.include(o)) {
+                result.add(o);
+            }
+        }
+        return result;
+    }
+    
+    default <R> SimpleList<R> map(Function<T, R> transform) {
+        SimpleList<R> result = new SimpleListImpl<>();
+        for (T item : this) {
+            result.add(transform.apply(item));
+        }
+        return result;
+    }
 }
--- src/test/java/ohm/softa/a04/tests/SimpleListTests.java
+++ src/test/java/ohm/softa/a04/tests/SimpleListTests.java
@@ -6,71 +6,93 @@ import ohm.softa.a04.SimpleListImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
 public class SimpleListTests {
-
-	private final Logger logger = LogManager.getLogger();
-	private SimpleList testList;
-
-	@BeforeEach
-	void setup(){
-		testList = new SimpleListImpl();
-
-		testList.add(1);
-		testList.add(2);
-		testList.add(3);
-		testList.add(4);
-		testList.add(5);
-	}
-
-	@Test
-	void testAddElements(){
-		logger.info("Testing if adding and iterating elements is implemented correctly");
-		int counter = 0;
-		for(Object o : testList){
-			counter++;
-		}
-		assertEquals(5, counter);
-	}
-
-	@Test
-	void testSize(){
-		logger.info("Testing if size() method is implemented correctly");
-		assertEquals(5, testList.size());
-	}
-
-	@Test
-	void testFilterAnonymousClass(){
-		logger.info("Testing the filter possibilities by filtering for all elements greater 2");
-		SimpleList result = testList.filter(new SimpleFilter() {
-			@Override
-			public boolean include(Object item) {
-				int current = (int)item;
-				return current > 2;
-			}
-		});
-
-		for(Object o : result){
-			int i = (int)o;
-			assertTrue(i > 2);
-		}
-	}
-
-	@Test
-	void testFilterLambda(){
-		logger.info("Testing the filter possibilities by filtering for all elements which are dividable by 2");
-		SimpleList result = testList.filter(o -> ((int) o) % 2 == 0);
-		for(Object o : result){
-			int i = (int)o;
-			assertTrue(i % 2 == 0);
-		}
-	}
+    
+    private final Logger logger = LogManager.getLogger();
+    private SimpleList<Integer> testList;
+    
+    @BeforeEach
+    void setup() {
+        testList = new SimpleListImpl<>();
+        
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        testList.add(4);
+        testList.add(5);
+    }
+    
+    @Test
+    @DisplayName("Testing if adding and iterating elements is implemented correctly")
+    void testAddElements() {
+        logger.info("Testing if adding and iterating elements is implemented correctly");
+        int counter = 0;
+        for (int i : testList) {
+            counter++;
+        }
+        assertEquals(5, counter);
+    }
+    
+    @Test
+    void testSize() {
+        logger.info("Testing if size() method is implemented correctly");
+        assertEquals(5, testList.size());
+    }
+    
+    @Test
+    void testFilterAnonymousClass() {
+        logger.info("Testing the filter possibilities by filtering for all elements greater 2");
+        SimpleList result = testList.filter(new SimpleFilter<Integer>() {
+            @Override
+            public boolean include(Integer item) {
+                return item > 2;
+            }
+        });
+        
+        for (Object o : result) {
+            int i = (int) o;
+            assertTrue(i > 2);
+        }
+    }
+    
+    @Test
+    void testFilterLambda() {
+        logger.info("Testing the filter possibilities by filtering for all elements which are dividable by 2");
+        SimpleList<Integer> result = testList.filter(i -> i % 2 == 0);
+        for (Integer i : result) {
+            assertTrue(i % 2 == 0);
+        }
+    }
+    
+    @Test
+    void get() {
+        IntStream.range(0, 5)
+                 .forEach((i) -> assertEquals((int) testList.get(i), i + 1));
+    }
+    
+    @Test
+    void getThrowsOutOfBounds() {
+        assertThrows(IndexOutOfBoundsException.class, () -> testList.get(5));
+    }
+    
+    @Test
+    void map() {
+        SimpleList<Integer> result = testList.map(i -> i * 2);
+        for (int i = 0; i < testList.size(); i++) {
+            int expected = testList.get(i) * 2;
+            int acutal = result.get(i);
+            assertEquals(expected, acutal);
+        }
+    }
 }
